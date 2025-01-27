a4f3ec16a12fb524ef33b91d054f7afb0cad959d
michele9812 <125456585+michele9812@users.noreply.github.com>
2025-01-27 19:18:48 +0100

    Update get-component.js

--- get-component.js
+++ get-component.js
@@ -1,3 +1,8 @@
+export default async function handler(req, res) {
+    // Risponde con un messaggio di test
+    res.status(200).json({ message: "Funzione API attiva!" });
+}
+
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
